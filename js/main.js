e8a76a362c591601bc9a945043fb911545570432
diegocardozo <diegokcm86@gmail.com>
2024-05-07 17:40:13 -0300

    inicio el repo

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//! chat GPT
+
+
+function calculadora() {
+    while (true) {
+        
+        let operacion = prompt("Ingrese la operación que desea realizar (+, -, *, /) o 'salir' para terminar:");
+
+        if (operacion === "salir"); {
+            console.log("hasta luego")
+            alert("¡Hasta luego!");
+            break;
+        }
+
+        let num1 = parseFloat(prompt("Ingrese el primer número:"));
+        let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+        switch (operacion) {
+            case '+':
+                alert("El resultado de la suma es: " + (num1 + num2));
+                break;
+            case '-':
+                alert("El resultado de la resta es: " + (num1 - num2));
+                break;
+            case '*':
+                alert("El resultado de la multiplicación es: " + (num1 * num2));
+                break;
+            case '/':
+                
+                if (num2 !== 0) {
+                    alert("El resultado de la división es: " + (num1 / num2));
+                } else {
+                    alert("¡No se puede dividir por cero!");
+                }
+                break;
+            default:
+                alert("Operación no válida.");
+        }
+    }
+}
+
+calculadora();
